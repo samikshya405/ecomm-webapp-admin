a16854ecfd2584c70314d263966ff456d4317634
samikshya <samikshyakharel405@gmail.com>
2024-02-27 20:45:49 +1100

    debug

--- src/Firebase/index.js
+++ src/Firebase/index.js
@@ -1,5 +1,5 @@
 import { initializeApp } from "firebase/app";
-import {getAuth} from 'firebase/auth'
+import { getAuth } from "firebase/auth";
 import { getFirestore } from "firebase/firestore";
 import { getStorage } from "firebase/storage";
 
@@ -10,8 +10,9 @@ const firebaseConfig = {
   storageBucket: import.meta.env.VITE_STORAGE_BUCKET,
   messagingSenderId: import.meta.env.VITE_MSG_SENDER_ID,
   appId: import.meta.env.VITE_APP_ID,
-  measurementId: import.meta.env.VITE_MEASUREMENT_ID
+  measurementId: import.meta.env.VITE_MEASUREMENT_ID,
 };
+console.log("firebaseConfig", firebaseConfig);
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
