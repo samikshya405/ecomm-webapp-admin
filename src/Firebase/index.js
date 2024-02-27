8e854093fd6a0b5e771f47d37310625ccd8b99e7
samikshya <samikshyakharel405@gmail.com>
2024-02-27 21:19:08 +1100

    Remove console log

--- src/Firebase/index.js
+++ src/Firebase/index.js
@@ -12,7 +12,7 @@ const firebaseConfig = {
   appId: import.meta.env.VITE_APP_ID,
   measurementId: import.meta.env.VITE_MEASUREMENT_ID,
 };
-console.log("firebaseConfig", firebaseConfig);
+
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
